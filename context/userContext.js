2e339a53d70d4f404b0abbc3629d184c1049f5b9
--replace-all <cogh0020@algonquinlive.com>
2022-12-05 14:44:27 -0500

    context wrapped around app and makes a fetch call to the API

--- context/userContext.js
+++ context/userContext.js
@@ -4,10 +4,20 @@ const Context = React.createContext()
 
 function ContextProvider(props) {
 
-  return <Context.Provider />
+  const [triviaQuestions, setTriviaQuestions] = React.useState([])
+  const [questionCount, setQuestionCount] = React.useState(5)
+
+  React.useEffect(() => {
+    fetch(`https://opentdb.com/api.php?amount=${questionCount}`)
+    .then(res => res.json())
+    .then(data =>{
+      setTriviaQuestions(data.results)
+      console.log(data.results)
+    })
+  }, [])
+
+  return <Context.Provider value={[triviaQuestions, questionCount, setQuestionCount]}{...props}/>
 }
-
-
 
 function useContext() {
   const context = React.useContext(Context)
