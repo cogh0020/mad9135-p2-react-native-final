37e2a20477e1a90c1acc5516a7f1e36e41d16b17
--replace-all <cogh0020@algonquinlive.com>
2022-12-05 13:53:25 -0500

    Added details screen boilerplate code

---
+++ components/Details.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+import {SafeAreaView} from 'react-native-safe-area-context'
+
+function Details({}){
+
+  return(
+    <SafeAreaView>
+      <View>
+        <Text></Text>
+      </View>
+    </SafeAreaView>
+  )
+}
---
+++ navigation/UserNavigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+const UserStackNavigation = () => {
+
+  return(
+    <Stack.Navigator>
+      
+    </Stack.Navigator>
+  )
+}
+
+export {UserStackNavigation}
